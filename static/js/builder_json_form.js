3787cb4f54dff51afc9ffd9c32c90b6d1b7cc61d
nieledm <nieledm@gmail.com>
2025-05-08 17:11:29 -0300

    Arrumando timestamp do arquivo json

--- static/js/builder_json_form.js
+++ static/js/builder_json_form.js
@@ -573,11 +573,18 @@ function generateAndValidateJSON() {
 }
 
 function downloadJSON(jsonData, baseFilename = 'alphafold_input') {
+  //Data e Hora local do Brasil
   const now = new Date();
-  const timestamp = now.toISOString()
-    .replace(/[:.]/g, '-')  // Substitui : e . por -
-    .replace('T', '_')      // Substitui T por _
-    .slice(0, 19);          // Pega YYYY-MM-DD_HH-MM-SS
+
+  const pad = (n) => n.toString().padStart(2, '0');
+
+  const year = now.getFullYear();
+  const month = pad(now.getMonth() + 1);
+  const day = pad(now.getDate());
+  const hours = pad(now.getHours());
+  const minutes = pad(now.getMinutes());
+  const seconds = pad(now.getSeconds());
+  const timestamp = `${year}-${month}-${day}_${hours}-${minutes}`;
   
   // Cria o nome do arquivo com timestamp
   const filename = `${baseFilename}_${timestamp}.json`;
